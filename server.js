d534cba701b76ef1e2a0088fbda840ccd30d1750
JOE-DP <joe.purvis@hotmail.co.uk>
2022-05-02 18:21:54 +0100

    second progress push - connect mongo db

--- server.js
+++ server.js
@@ -1,4 +1,41 @@
+// const { urlencoded } = require('body-parser')
+// const { Console } = require('console')
 const express = require('express')
 const http = require('http')
+const PORT = 5001
+const MongoClient = require('mongodb').MongoClient
 
-let touch app = express()
+let db;
+let dbConnectorStr = 'mongodb+srv://fitnessAdmin:fitnesspass@cluster0.dh91y.mongodb.net/fitnesstracker?retryWrites=true&w=majority'
+let dbName = 'fitnessDB'
+
+MongoClient.connect(dbConnectorStr)
+    .then(client => {
+        console.log(`connected to ${dbName} database`)
+        db = client.db(dbName)
+    })
+
+
+let app = express()
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended : true}))
+app.use(express.json())
+
+
+app.get('/', (req, res) => {
+    res.render('index.ejs')
+})
+
+app.post('/add-ex', (req, res) => {
+    db.collection('fitnessuser1').insertOne(req.body)
+    res.redirect('/')
+})
+
+
+
+
+
+
+app.listen(process.env.PORT|| PORT, () => {
+    console.log(`server running on port: ${PORT}`)
+})
